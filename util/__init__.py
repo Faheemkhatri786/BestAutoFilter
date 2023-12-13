b75361dea549c860aa2596ad50dae66a022183a2
DevAryanxDD <144301993+DevAryanxDD@users.noreply.github.com>
2023-12-13 08:09:34 +0530

    Add files via upload

---
+++ util/__init__.py
@@ -0,0 +1,2 @@
+
+# Don't Delete Else Error !!
---
+++ util/file_size.py
@@ -0,0 +1,2 @@
+def human_size(bytes, units=[' bytes','KB','MB','GB','TB', 'PB', 'EB']):
+    return str(bytes) + units[0] if int(bytes) < 1024 else human_size(int(bytes)>>10, units[1:])
